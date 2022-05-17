02536dccb3c2fbb5dd68d5672016971214d95da0
KFTamang <subsonic.detonation@gmail.com>
2022-05-17 23:58:37 +0900

    split main.rs into main.rs and cpu.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
+mod cpu;
+use crate::cpu::*;
+
 use std::env;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 
-// dram memory size, 128MB
-pub const DRAM_SIZE: u64 = 1024 * 1024 * 128;
-
-struct Cpu {
-    regs: [u64; 32],
-    pc: u64,
-    dram: Vec<u8>,
-}
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
@@ -33,85 +28,4 @@ fn main() -> io::Result<()> {
     }
 
     Ok(())
-}
-
-impl Cpu {
-    fn new(code: Vec<u8>) -> Self {
-        let mut regs = [0; 32];
-        regs[2] = DRAM_SIZE;
-        Self {
-            regs,
-            pc: 0,
-            dram: code,
-        }
-    }
-
-    fn fetch(&self) -> u32 {
-        let index = self.pc as usize;
-        return ((self.dram[index + 0] as u32) << 0)
-            | ((self.dram[index + 1] as u32) << 8)
-            | ((self.dram[index + 2] as u32) << 16)
-            | ((self.dram[index + 3] as u32) << 24);
-    }
-    fn execute(&mut self, inst: u32) {
-        let opcode = inst & 0x7f;
-        let rd = ((inst >> 7) & 0x1f) as usize;
-        let rs1 = ((inst >> 15) & 0x1f) as usize;
-        let rs2 = ((inst >> 20) & 0x1f) as usize;
-
-        match opcode {
-            0x33 => {
-                // add
-                println!(
-                    "opcode:{}({}), rd:{}, rs1:{}, rs2:{}",
-                    opcode, "add", rd, rs1, rs2
-                );
-                self.regs[rd] = self.regs[rs1].wrapping_add(self.regs[rs2]);
-            }
-            0x13 => {
-                // addi
-                println!(
-                    "opcode:{}({}), rd:{}, rs1:{}, rs2:{}",
-                    opcode, "addi", rd, rs1, rs2
-                );
-                let imm = ((inst >> 20) & 0xfff) as u64;
-                self.regs[rd] = self.regs[rs1].wrapping_add(imm);
-            }
-            _ => {
-                dbg!("not implemented yet!");
-            }
-        }
-    }
-
-    fn dump_registers(&self) {
-        let mut output = String::from("");
-        let abi = [
-            "zero", " ra ", " sp ", " gp ", " tp ", " t0 ", " t1 ", " t2 ", " s0 ", " s1 ", " a0 ",
-            " a1 ", " a2 ", " a3 ", " a4 ", " a5 ", " a6 ", " a7 ", " s2 ", " s3 ", " s4 ", " s5 ",
-            " s6 ", " s7 ", " s8 ", " s9 ", " s10", " s11", " t3 ", " t4 ", " t5 ", " t6 ",
-        ];
-        output = format!("pc={:>#18x}", self.pc);
-        for i in (0..32).step_by(4) {
-            output = format!(
-                "{}\n{}",
-                output,
-                format!(
-                    "x{:02}({})={:>#18x}, x{:02}({})={:>#18x}, x{:02}({})={:>#18x}, x{:02}({})={:>#18x}",
-                    i,
-                    abi[i],
-                    self.regs[i],
-                    i+1,
-                    abi[i+1],
-                    self.regs[i+1],
-                    i+2,
-                    abi[i+2],
-                    self.regs[i+2],
-                    i+3,
-                    abi[i+3],
-                    self.regs[i+3]
-                )
-            )
-        }
-        println!("{}", output);
-    }
 }

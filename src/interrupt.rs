f58c49cb94a76230686adc17599f883d39fe2d3b
KFTamang <subsonic.detonation@gmail.com>
2022-07-31 22:42:59 +0900

    [WIP] add csr manipulation to interrupt

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -1,3 +1,5 @@
+use crate::csr::*;
+
 pub struct Interrupt {
     pending_interrupt: Option<u32>,
 }
@@ -11,5 +13,9 @@ impl Interrupt {
 
     pub fn get_pending_interrupt(&self) -> Option<u32> {
         self.pending_interrupt
+    }
+
+    pub fn set_pending_interrupt(&mut self, csr: &mut Csr, i: u32) {
+        self.pending_interrupt = Some(i);
     }
 }

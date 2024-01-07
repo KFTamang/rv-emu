584e9edfc587ae41e23bb03a5554cedae34d762c
KFTamang <subsonic.detonation@gmail.com>
2024-01-08 00:25:53 +0900

    Change target architechture to RISC-V

--- src/debugger.rs
+++ src/debugger.rs
@@ -21,7 +21,7 @@ use gdbstub::stub::SingleThreadStopReason;
 
 impl Target for Emu {
     type Error = ();
-    type Arch = gdbstub_arch::arm::Armv4t; // as an example
+    type Arch = gdbstub_arch::riscv::Riscv64; // as an example
 
     #[inline(always)]
     fn base_ops(&mut self) -> BaseOps<Self::Arch, Self::Error> {
@@ -38,23 +38,23 @@ impl Target for Emu {
 impl SingleThreadBase for Emu {
     fn read_registers(
         &mut self,
-        regs: &mut gdbstub_arch::arm::reg::ArmCoreRegs,
+        regs: &mut gdbstub_arch::riscv::reg::RiscvCoreRegs<u64>,
     ) -> TargetResult<(), Self> { todo!() }
 
     fn write_registers(
         &mut self,
-        regs: &gdbstub_arch::arm::reg::ArmCoreRegs
+        regs: &gdbstub_arch::riscv::reg::RiscvCoreRegs<u64>
     ) -> TargetResult<(), Self> { todo!() }
 
     fn read_addrs(
         &mut self,
-        start_addr: u32,
+        start_addr: u64,
         data: &mut [u8],
     ) -> TargetResult<usize, Self> { todo!() }
 
     fn write_addrs(
         &mut self,
-        start_addr: u32,
+        start_addr: u64,
         data: &[u8],
     ) -> TargetResult<(), Self> { todo!() }
 
@@ -101,14 +101,14 @@ impl Breakpoints for Emu {
 impl SwBreakpoint for Emu {
     fn add_sw_breakpoint(
         &mut self,
-        addr: u32,
-        kind: gdbstub_arch::arm::ArmBreakpointKind,
+        addr: u64,
+        kind: usize,
     ) -> TargetResult<bool, Self> { todo!() }
 
     fn remove_sw_breakpoint(
         &mut self,
-        addr: u32,
-        kind: gdbstub_arch::arm::ArmBreakpointKind,
+        addr: u64,
+        kind: usize,
     ) -> TargetResult<bool, Self> { todo!() }
 }
 
@@ -135,7 +135,7 @@ impl run_blocking::BlockingEventLoop for MyGdbBlockingEventLoop {
     type Connection = Box<dyn ConnectionExt<Error = std::io::Error>>;
 
     // or MultiThreadStopReason on multi threaded targets
-    type StopReason = SingleThreadStopReason<u32>;
+    type StopReason = SingleThreadStopReason<u64>;
 
     // Invoked immediately after the target's `resume` method has been
     // called. The implementation should block until either the target
@@ -144,7 +144,7 @@ impl run_blocking::BlockingEventLoop for MyGdbBlockingEventLoop {
         target: &mut Emu,
         conn: &mut Self::Connection,
     ) -> Result<
-        run_blocking::Event<SingleThreadStopReason<u32>>,
+        run_blocking::Event<SingleThreadStopReason<u64>>,
         run_blocking::WaitForStopReasonError<
             <Self::Target as Target>::Error,
             <Self::Connection as Connection>::Error,
@@ -188,7 +188,7 @@ impl run_blocking::BlockingEventLoop for MyGdbBlockingEventLoop {
     // Invoked when the GDB client sends a Ctrl-C interrupt.
     fn on_interrupt(
         target: &mut Emu,
-    ) -> Result<Option<SingleThreadStopReason<u32>>, <Emu as Target>::Error> {
+    ) -> Result<Option<SingleThreadStopReason<u64>>, <Emu as Target>::Error> {
         // notify the target that a ctrl-c interrupt has occurred.
 
         // a pretty typical stop reason in response to a Ctrl-C interrupt is to

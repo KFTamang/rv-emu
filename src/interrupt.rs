96cf363354c6c103b6e642d823210198a8446a0a
Kento Furukawa <kentofurukawa4@gmail.com>
2025-03-22 07:49:08 +0900

    Fix log format

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -52,8 +52,8 @@ impl Interrupt {
                 cpu.csr.set_mstatus_bit(0, MASK_MIE, MASK_MIE);
 
                 let mtvec = cpu.csr.load_csrs(MTVEC);
-                info!("mtvec is 0x{:x}\n", mtvec);
-                info!("enter M mode\n");
+                info!("mtvec is 0x{:x}", mtvec);
+                info!("enter M mode");
                 match mtvec & 0x3 {
                     0x0 => {
                         cpu.pc = (mtvec & 0xfffffffc).wrapping_sub(4);
